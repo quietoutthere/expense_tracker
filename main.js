adb8322aed77a7d63ad0622563c69f74a7804121
Evan Oyster <oyster.evan@gmail.com>
2022-10-31 10:17:00 -0400

    expense node created

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-const runExpenses = document.getElementById('button');
+const form = document.getElementById('form');
 
-runExpenses.addEventListener('click', (e)=> {
+form.addEventListener('submit', function(e) {
     e.preventDefault();
-    const expenseInput = document.getElementById('expense-table');
+    createNewRow();
+});
+
+function grabExpenseValues() {
+    const type = document.getElementById('type').value;
     const name = document.getElementById('name').value;
-    const nameOfExpense = document.createElement('td');
-    nameOfExpense.appendChild(expenseInput); 
-})
+    const date = document.getElementById('date').value;
+    const amount = document.getElementById('amount').value;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = 'Delete';
+    // deleteBtn.addEventListener('click', function(e) {
+    //     const target = e.target;
+    //     console.log(target);
+    // });
+
+    return [type, name, date, amount, deleteBtn];
+    
+}
+
+function createNewRow() {
+    let expenseValues = grabExpenseValues();
+
+    const expenseNode1 = document.createTextNode(expenseValues[0]);
+    const expenseNode2 = document.createTextNode(expenseValues[1]);
+    const expenseNode3 = document.createTextNode(expenseValues[2]);
+    const expenseNode4 = document.createTextNode(expenseValues[3]);
+    
+    const cell = document.createElement('td');
+    const row = document.createElement('tr');
+   
+    const table = document.getElementById('table');
+    console.log(expenseValues[0])
+}
